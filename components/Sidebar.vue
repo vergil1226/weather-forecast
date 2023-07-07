2de0d0b743cd6a9493385448ad10498affe38477
Brittany <brittany@overflowmarketing.com>
2023-07-07 13:13:23 -0400

    Added sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      class="deep-purple accent-4"
+      dark
+      permanent
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
