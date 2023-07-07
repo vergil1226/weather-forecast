351055905e9a22a8008006f25c698f4f11faf7b1
Brittany <brittany@overflowmarketing.com>
2023-07-07 13:10:19 -0400

    Deleted information no longer needed

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+       
+</template>
+
+<script>
+import Vue from 'vue'
+import VTooltip from 'v-tooltip'
+export default {
+  name: 'IndexPage'
+}
+</script>
