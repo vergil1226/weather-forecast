16237a62d8f9eb9e2a284633427fa46430321db1
Brittany <brittany@overflowmarketing.com>
2023-07-12 10:35:41 -0400

    Updated icons for forecast cards

--- components/FiveDayForecastCards.vue
+++ components/FiveDayForecastCards.vue
@@ -1,41 +1,36 @@
 <template>
-  <div>
-    <v-card class="forecast-card" elevation="0" color="#5096FF">
-      <h3 class="forecast-card__title">5-Day Forecast</h3>
-      <div class="d-flex">
-        <ol class="forecast-card-list">
-          <li class="forecast-card-list__items">
-            <v-card class="forecast-card">
-              <v-container>
-                <v-row>
-                  <v-col cols="12">
-                    <h2 class="forecast-card__title">5-Day Forecast</h2>
-                    <p class="forecast-card-day">Monday</p>
-                    <v-icon>mdi-google-circles-extended</v-icon>
-                    <p class="forecast-card__range">13</p>
-                    <p class="forecast-card__range">10</p>
-                  </v-col>
-                  <v-col cols="12">
-                    <h2 class="forecast-card__title">5-Day Forecast</h2>
-                    <p class="forecast-card-day">Tuesday</p>
-                    <v-icon>mdi-google-circles-extended</v-icon>
-                    <p class="forecast-card__range">17</p>
-                    <p class="forecast-card__range">12</p>
-                  </v-col>
-                  <v-col cols="12">
-                    <h2 class="forecast-card__title">5-Day Forecast</h2>
-                    <p class="forecast-card-day">Wednesday</p>
-                    <v-icon>mdi-google-circles-extended</v-icon>
-                    <p class="forecast-card__range">21</p>
-                    <p class="forecast-card__range">18</p>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card>
-          </li>
-        </ol>
-      </div>
-    </v-card>
+  <div class="d-flex">
+    <ol class="forecast-card-list">
+      <li class="forecast-card-list__items">
+        <v-card class="forecast-card">
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <h2 class="forecast-card__title">5-Day Forecast</h2>
+                <p class="forecast-card-day">Monday</p>
+                <v-icon>mdi-google-circles-extended</v-icon>
+                <p class="forecast-card__range">13</p>
+                <p class="forecast-card__range">10</p>
+              </v-col>
+              <v-col cols="12">
+                <h2 class="forecast-card__title">5-Day Forecast</h2>
+                <p class="forecast-card-day">Tuesday</p>
+                <v-icon>mdi-weather-lightning</v-icon>
+                <p class="forecast-card__range">17</p>
+                <p class="forecast-card__range">12</p>
+              </v-col>
+              <v-col cols="12">
+                <h2 class="forecast-card__title">5-Day Forecast</h2>
+                <p class="forecast-card-day">Wednesday</p>
+                <v-icon>mdi-weather-partly-cloudy</v-icon>
+                <p class="forecast-card__range">21</p>
+                <p class="forecast-card__range">18</p>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -46,6 +41,8 @@ export default {};
 <style lang="scss" scoped>
 .forecast-card {
   background-color: #104084;
+  width: 455px;
+  height: 256px;
   &__title {
     font-family: SF Pro Display;
     font-size: 20px;
@@ -64,14 +61,14 @@ export default {};
     text-align: left;
     color: #ffffff;
   }
-  &__range{
+  &__range {
     font-family: Alegreya Sans;
     font-size: 18px;
     font-weight: 500;
     line-height: 22px;
     letter-spacing: 0em;
     text-align: center;
-    color: #FFFFFF;
+    color: #ffffff;
   }
 }
 </style>
